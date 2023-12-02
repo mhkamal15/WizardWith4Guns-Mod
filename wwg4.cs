03ca304b53d74fb113357c69b5f0b72300207564
Malek <120230397+mhkamal15@users.noreply.github.com>
2023-12-02 14:09:19 -0500

    Update wwg4.cs

--- wwg4.cs
+++ wwg4.cs
@@ -1,15 +1,19 @@
-//Declare necessary using statements
 using UnityEngine;
 using System.Reflection;
 
-//Declare the mod class.
+//mod class.
 public class WizardWith4Guns : BaseUnityPlugin {
 
-    //Start method runs on enabling the mod.
+    //Start based on enabling.
     void Start() {
-        //Find the necessary class using reflection.
-        Type coOpClass = Type.GetType("Assembly-CSharp.CoOpClass");
-        MethodInfo methodInfo = coOpClass.GetMethod("Enable4PlayerCoOp", BindingFlags.NonPublic | BindingFlags.Instance);
+        //Decompile Assembly-CSharp.dll using a decompiler tool
+        //Identify the necessary class and method responsible for co-op gameplay
+        //Update the class and method names accordingly in the following code
+        
+        //Finding necessary class.
+        Type coOpClass = Type.GetType("Assembly_CSharp.Coop.CoOpClass");
+        MethodInfo methodInfo = coOpClass.GetMethod("Enable2PlayerCoOp", BindingFlags.NonPublic | BindingFlags.Instance);
+        
         //Patch the method to enable 4-player co-op.
         MonoMod.RuntimeDetour.HookGen.HookEndpointManager.Modify(methodInfo, delegate(MonoMod.RuntimeDetour.HookGen.HookMethodDelegate orig, object o) {
             //Change the maximum player count to 4.
